364791464f70fd33cacebbfb6ef14750717ae8bc
Radhe Pokhrel <rshyampokhrel@gmail.com>
2021-11-14 16:42:30 -0500

    Created File for display class and aftwork objects

---
+++ DisplayUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpaceGame
+{
+    class Display
+    {
+        private List<string> imageList = new List<string> { };
+        public string GetInput()
+        {
+            return "";
+        }
+        public void Draw()
+        {
+
+        }
+        public void Write(string text)
+        {
+            Console.Write(text);
+        }
+    }
+}
